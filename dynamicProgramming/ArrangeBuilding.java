23c12c4f3c3d97d07bec30fd8dfc1e13707307bb
unknown <ronit2001krish@gmail.com>
2021-09-23 14:40:52 +0530

    meessage

--- dynamicProgramming/ArrangeBuilding.java
+++ dynamicProgramming/ArrangeBuilding.java
@@ -7,15 +7,15 @@ public class ArrangeBuilding {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        int send=1;
-        int bend =1;
-        for(int i=2;i<=n;i++){
-            int Nsend=bend;
-            int Nbend=Nsend+send;
+        long send = 1;
+        long bend = 1;
+        for (int i = 2; i <= n; i++) {
+            long Nsend = bend;
+            long Nbend = bend + send;
 
-            send=Nsend;
-            bend=Nbend;
+            send = Nsend;
+            bend = Nbend;
         }
-        System.out.println((send+bend)*(send+bend));
+        System.out.println((send + bend) * (send + bend));
     }
 }
