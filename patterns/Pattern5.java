d9a4417c0f4284107cc236f164071067b643cd32
unknown <ronit2001krish@gmail.com>
2021-09-25 21:16:26 +0530

    pattern ques

---
+++ patterns/Pattern5.java
@@ -0,0 +1,30 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        int sp = n / 2;
+        int st = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < sp; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 0; j < st; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+            if (i < n / 2) {
+                st += 2;
+                sp--;
+            } else {
+                st -= 2;
+                sp++;
+            }
+        }
+    }
+}
