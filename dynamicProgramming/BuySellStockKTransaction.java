f9e292c40f13ee3d5c33b0152c339002f45df06f
unknown <ronit2001krish@gmail.com>
2021-09-30 19:08:26 +0530

    dp done _/

--- dynamicProgramming/BuySellStockKTransaction.java
+++ dynamicProgramming/BuySellStockKTransaction.java
@@ -12,17 +12,23 @@ public class BuySellStockKTransaction {
         int k = sc.nextInt();
         sc.close();
         int[][] dp = new int[k + 1][n];
-        for (int i = 1; i <= k; i++) {
-            int max = dp[i - 1][0];
+        for (int i = 1; i < dp.length; i++) {
+            int max = dp[i][0] - price[0];
             for (int j = 1; j < n; j++) {
-                max = Math.max(max, dp[i - 1][j] - price[j-1]);
-                if(max+price[j]>dp[i-1][0]){
-                    dp[i][j]=max+price[j];
-                }else{
-                    dp[i][j]=dp[i-1][j-1];
+                if (j > 1) {
+                    max = Math.max(max, dp[i - 1][j - 1] - price[j - 1]);
+                }
+                if (max + price[j] > dp[i][j - 1]) {
+                    dp[i][j] = max + price[j];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
                 }
             }
-            System.out.println(dp[k][n-1]);
+        }
+        for (int i = 0; i <= k; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(dp[i][j] + "\t");
+            System.out.println();
         }
     }
 }
