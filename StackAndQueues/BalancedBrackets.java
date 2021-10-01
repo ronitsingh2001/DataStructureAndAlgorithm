8c46bf2b6621c089c45e7efd1f59d2352ecb07f9
unknown <ronit2001krish@gmail.com>
2021-10-01 21:07:58 +0530

    stacks

--- StackAndQueues/BalancedBrackets.java
+++ StackAndQueues/BalancedBrackets.java
@@ -11,31 +11,34 @@ public class BalancedBrackets {
 
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-           if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 st.push(s.charAt(i));
-           }else if(s.charAt(i)==')'){
-               if(st.size()==0||st.peek()!='('){
-                   System.out.println(false);
-                   return;
-               }else{
-                   st.pop();
-               }
-           }else if(s.charAt(i)=='}'){
-            if(st.size()==0||st.peek()!='{'){
-                System.out.println(false);
-                return;
-            }else{
-                st.pop();
+            } else if (s.charAt(i) == ')') {
+                if (st.size() == 0 || st.peek() != '(') {
+                    System.out.println(false);
+                    return;
+                } else {
+                    st.pop();
+                }
+            } else if (s.charAt(i) == '}') {
+                if (st.size() == 0 || st.peek() != '{') {
+                    System.out.println(false);
+                    return;
+                } else {
+                    st.pop();
+                }
+            } else if (s.charAt(i) == ']') {
+                if (st.size() == 0 || st.peek() != '[') {
+                    System.out.println(false);
+                    return;
+                } else {
+                    st.pop();
+                }
             }
-           }else if(s.charAt(i)==']'){
-            if(st.size()==0||st.peek()!='['){
-                System.out.println(false);
-                return;
-            }else{
-                st.pop();
-            }
-           }
         }
-        System.out.println(true);
+        if (st.size() != 0) {
+            System.out.println(false);
+        } else
+            System.out.println(true);
     }
 }
