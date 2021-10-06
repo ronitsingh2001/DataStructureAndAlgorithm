df297e1c17fe71ea6177a28b33e1d359c24b0776
unknown <ronit2001krish@gmail.com>
2021-10-07 00:31:52 +0530

    infix prefix postfix ques

---
+++ StackAndQueues/InfixConversion.java
@@ -0,0 +1,64 @@
+package StackAndQueues;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class InfixConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+        Stack<String> pre = new Stack<>();
+        Stack<String> post = new Stack<>();
+        Stack<Character> oper = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                oper.push(ch);
+            }else if(Character.isLetter(ch)){
+                pre.push(ch+"");
+                post.push(ch+"");
+          }else if(ch == '+'||ch=='-'||ch=='*'||ch=='/'){
+                while(oper.size()>0&&precedence(ch) <= precedence(oper.peek()) && oper.peek()!='('){
+                    char op = oper.pop();
+                    String v2 = pre.pop();
+                    String v1 = pre.pop();
+                    pre.push(op+v1+v2);
+                    v2=post.pop();
+                    v1=post.pop();
+                    post.push(v1+v2+op);
+                }
+                oper.push(ch);
+            }else if(ch == ')'){
+                while(oper.peek()!='('){
+                    char op = oper.pop();
+                    String v2 = pre.pop();
+                    String v1 = pre.pop();
+                    pre.push(op+v1+v2);
+                    v2=post.pop();
+                    v1=post.pop();
+                    post.push(v1+v2+op);
+                }
+                oper.pop();
+            }
+        }
+        while(oper.size()>0){
+            char op = oper.pop();
+            String v2 = pre.pop();
+            String v1 = pre.pop();
+            pre.push(op+v1+v2);
+            v2=post.pop();
+            v1=post.pop();
+            post.push(v1+v2+op);
+        }
+        System.out.println(post.pop());
+        System.out.println(pre.pop());
+    }
+
+    public static int precedence(char ch) {
+        if (ch == '+' || ch == '-')
+            return 1;
+        else
+            return 2;
+    }
+}
---
+++ StackAndQueues/PostfixToInfixAndPreFix.java
@@ -0,0 +1,47 @@
+package StackAndQueues;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PostfixToInfixAndPreFix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+        Stack<String> pre=new Stack<>();
+        Stack<Integer> eval=new Stack<>();
+        Stack<String> in=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                in.push(ch+"");
+                pre.push(ch+"");
+                eval.push(ch-'0');
+            }else if(ch=='+'||ch=='-'||ch=='*'||ch=='/'){
+                String v2 = in.pop();
+                String v1 = in.pop();
+                in.push('('+v1+ch+v2+')');
+                v2=pre.pop();
+                v1=pre.pop();
+                pre.push(ch+v1+v2);
+                int o2=eval.pop();
+                int o1=eval.pop();
+                eval.push(evaluation(o1,o2,ch));
+            }
+        }
+        System.out.println(eval.pop());
+        System.out.println(in.pop());
+        System.out.println(pre.pop());
+    }
+    public static int evaluation(int v1 , int v2 , char op){
+        if(op=='+'){
+            return v1+v2;
+        }else if(op=='-'){
+            return v1-v2;
+        }else if(op=='*'){
+            return v1*v2;
+        }else{
+            return v1/v2;
+        }
+    }
+}
