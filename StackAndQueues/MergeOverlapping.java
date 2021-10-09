e06ddf3d6e01e79a6aba6ac9e4e9e033702039ac
unknown <ronit2001krish@gmail.com>
2021-10-09 22:35:07 +0530

    stacks

---
+++ StackAndQueues/MergeOverlapping.java
@@ -0,0 +1,25 @@
+package StackAndQueues;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MergeOverlapping {
+    public static void main(String[] args) throws Exception {
+       
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+
+        for (int j = 0; j < n; j++) {
+            String line = br.readLine();
+            arr[j][0] = Integer.parseInt(line.split(" ")[0]);
+            arr[j][1] = Integer.parseInt(line.split(" ")[1]);
+        }
+
+        mergeOverlappingIntervals(arr);
+    }
+
+    public static void mergeOverlappingIntervals(int[][] arr) {
+       
+    }
+}
