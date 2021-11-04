f196804a3c186a966030e9d4fbb15d5902f854ed
unknown <ronit2001krish@gmail.com>
2021-11-04 23:52:35 +0530

    LL

---
+++ patterns/Pattern17.java
@@ -0,0 +1,20 @@
+package patterns;
+import java.util.*;
+public class Pattern17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==n/2||j==n/2){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
