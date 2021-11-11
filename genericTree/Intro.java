e5770d84b0739055218a1ffaf5f1ed9304c677fa
unknown <ronit2001krish@gmail.com>
2021-11-11 23:27:54 +0530

    LL

---
+++ genericTree/Intro.java
@@ -0,0 +1,15 @@
+package genericTree;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class Intro {
+    public class Node {
+        int data;
+        ArrayList<Node> Children = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Node root;
+    }
+}
