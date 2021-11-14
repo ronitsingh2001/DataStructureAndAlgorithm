a658dc987d18e02f0ce3f997135118be2fd381e4
unknown <ronit2001krish@gmail.com>
2021-11-14 13:52:10 +0530

    p17

--- patterns/Pattern17.java
+++ patterns/Pattern17.java
@@ -1,20 +1,27 @@
 package patterns;
+
 import java.util.*;
+
 public class Pattern17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==n/2||j==n/2){
-                    System.out.print("*\t");
-                }else{
+        int st = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                if (i != n / 2)
                     System.out.print("\t");
-                }
-                
+                else
+                    System.out.print("*\t");
             }
+            for (int j = 0; j < st; j++)
+                System.out.print("*\t");
             System.out.println();
+            if (i < n / 2)
+                st++;
+            else
+                st--;
         }
     }
 }
