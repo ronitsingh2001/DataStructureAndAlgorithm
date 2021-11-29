2badb5e7018991099cee9d2c8dc3c88c8b1c6245
unknown <ronit2001krish@gmail.com>
2021-11-29 16:10:57 +0530

    optimised

--- timeAndSpaceComplex/SortDate.java
+++ timeAndSpaceComplex/SortDate.java
@@ -3,52 +3,51 @@ package timeAndSpaceComplex;
 import java.util.Scanner;
 
 public class SortDate {
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        String[] a = new String[n];
-        for (int i = 0; i < n; i++) {
-            String str = scn.next();
-            a[i] = str;
-        }
-        scn.close();
 
-        sortDates(a);
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    String[] a = new String[n];
+    for (int i = 0; i < n; i++) {
+      String str = scn.next();
+      a[i] = str;
+    }
+    scn.close();
 
-        print(a);
+    sortDates(a);
+
+    print(a);
+  }
+
+  public static void sortDates(String[] a) {
+    countSort(a, 1000000, 100, 32);
+    countSort(a, 10000, 100, 13);
+    countSort(a, 1, 10000, 2501);
+  }
+
+  public static void countSort(String[] a, int div, int mod, int range) {
+    int[] freq = new int[range];
+    for (int i = 0; i < a.length; i++) freq[Integer.parseInt(a[i], 10) /
+      div %
+      mod]++;
+
+    for (int i = 1; i < freq.length; i++) freq[i] += freq[i - 1];
+
+    String[] ans = new String[a.length];
+
+    for (int i = a.length - 1; i >= 0; i--) {
+      int pos = freq[Integer.parseInt(a[i], 10) / div % mod] - 1;
+      ans[pos] = a[i];
+      freq[Integer.parseInt(a[i], 10) / div % mod]--;
     }
 
-    public static void sortDates(String[] a) {
-        countSort(a, 1000000, 100, 32);
-        countSort(a, 10000, 100, 13);
-        countSort(a, 1, 10000, 2501);
+    for (int i = 0; i < a.length; i++) a[i] = ans[i];
+  }
+
+  public static void print(String[] a) {
+    System.out.println();
+    for (int i = 0; i < a.length; i++) {
+      System.out.println(a[i]);
     }
-
-    public static void countSort(String[] a, int div, int mod, int range) {
-        int[] freq = new int[range];
-        for (int i = 0; i < a.length; i++)
-            freq[Integer.parseInt(a[i], 10) / div % mod]++;
-
-        for (int i = 1; i < freq.length; i++)
-            freq[i] += freq[i - 1];
-
-        String[] ans = new String[a.length];
-
-        for (int i = a.length-1; i >=0; i--) {
-            int pos = freq[Integer.parseInt(a[i], 10) / div % mod] - 1;
-            ans[pos] = a[i];
-            freq[Integer.parseInt(a[i], 10) / div % mod]--;
-        }
-
-        for (int i = 0; i < a.length; i++)
-            a[i] = ans[i];
-    }
-
-    public static void print(String[] a) {
-        System.out.println();
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
-    }
-
+  }
 }
