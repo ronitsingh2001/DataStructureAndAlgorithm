f6b29696ee73cbf39fcc93920df892c24f094160
unknown <ronit2001krish@gmail.com>
2021-12-02 19:47:59 +0530

    gt

--- genericTree/DisplayGenericTree.java
+++ genericTree/DisplayGenericTree.java
@@ -1,7 +1,6 @@
 package genericTree;
 
 public class DisplayGenericTree {
-    public static void main(String[] args) {
-        
-    }
+
+  public static void main(String[] args) {}
 }
--- genericTree/Intro.java
+++ genericTree/Intro.java
@@ -1,15 +1,18 @@
 package genericTree;
 
 import java.util.ArrayList;
+
 // import java.util.*;
 
 public class Intro {
-    public class Node {
-        int data;
-        ArrayList<Node> Children = new ArrayList<>();
-    }
 
-    public static void main(String[] args) {
-        // Node root;
-    }
+  public class Node {
+
+    int data;
+    ArrayList<Node> Children = new ArrayList<>();
+  }
+
+  public static void main(String[] args) {
+    // Node root;
+  }
 }
