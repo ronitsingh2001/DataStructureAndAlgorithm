d21ee66f0505a5731835a71d14e3ca46d1a9c7d9
unknown <ronit2001krish@gmail.com>
2021-12-13 17:42:03 +0530

    patterns

--- patterns/Pattern4.java
+++ patterns/Pattern4.java
@@ -3,19 +3,17 @@ package patterns;
 import java.util.Scanner;
 
 public class Pattern4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
 
-        for (int i = n; i > 0; i--) {
-            for (int j = n; j > 0; j--) {
-                if (i >= j)
-                    System.out.print("*\t");
-                else
-                    System.out.print("\t");
-            }
-            System.out.println();
-        }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+
+    for (int i = n; i > 0; i--) {
+      for (int j = n; j > 0; j--) {
+        if (i >= j) System.out.print("*\t"); else System.out.print("\t");
+      }
+      System.out.println();
     }
+  }
 }
