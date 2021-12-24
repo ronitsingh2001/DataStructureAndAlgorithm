8529e832e8a1be33312cb8665a1fb67a608c73a5
unknown <ronit2001krish@gmail.com>
2021-12-24 23:27:03 +0530

    patterns

--- patterns/Pattern7.java
+++ patterns/Pattern7.java
@@ -3,19 +3,16 @@ package patterns;
 import java.util.Scanner;
 
 public class Pattern7 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (i == j)
-                    System.out.print("*\t");
-                else
-                    System.out.print("\t");
-            }
-            System.out.println();
-        }
-    }
 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j <= i; j++) {
+        if (i == j) System.out.print("*\t"); else System.out.print("\t");
+      }
+      System.out.println();
+    }
+  }
 }
