798ebe7b5832451befa685fb3c2161f492261b0c
unknown <ronit2001krish@gmail.com>
2022-02-16 16:43:21 +0530

    rotate a num

--- gettingStarted/RotateANum.java
+++ gettingStarted/RotateANum.java
@@ -3,11 +3,23 @@ package gettingStarted;
 import java.util.Scanner;
 
 public class RotateANum {
-    public static void main(String[] agrs){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int rot=sc.nextInt();
-        sc.close();
-       
+
+  public static void main(String[] agrs) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int rot = sc.nextInt();
+    sc.close();
+    int temp = n, nod = 0, res = n;
+    while (temp > 0) {
+      nod++;
+      temp /= 10;
     }
+    rot = rot % nod;
+    if (rot < 0) rot += nod;
+
+    int p1 = (int) (n % Math.pow(10, rot));
+    n = (int) (n / Math.pow(10, rot));
+    res = (int) (p1 * Math.pow(10, nod - rot)) + n;
+    System.out.println(res);
+  }
 }
