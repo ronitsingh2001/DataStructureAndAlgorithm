c82b057abb997fe1b4017890b550a3d6d40f8d1b
unknown <ronit2001krish@gmail.com>
2022-02-17 14:26:05 +0530

    new qques

---
+++ gettingStarted/PythagorasTriplet.java
@@ -0,0 +1,17 @@
+package gettingStarted;
+
+import java.util.Scanner;
+
+public class PythagorasTriplet {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    sc.close();
+    if (a*a + b*b == c * c || b*b + c*c == a * a || a*a + c*c == b * b) System.out.println(
+      true
+    ); else System.out.println(false);
+  }
+}
