06d388719add88de17e9ffcac7cb93c6032277f9
unknown <ronit2001krish@gmail.com>
2022-03-01 13:48:30 +0530

    new ques

--- gettingStarted/PrimeFactor.java
+++ gettingStarted/PrimeFactor.java
@@ -3,15 +3,16 @@ package gettingStarted;
 import java.util.Scanner;
 
 public class PrimeFactor {
-    public static void main(String[] agrs){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.close();
-        for(int i=2;i<=n&&n>1;){
-            if(n%i==0){
-                System.out.print(i+" ");
-                n/=i;
-            }else i++;
-        }
+
+  public static void main(String[] agrs) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.close();
+    for (int i = 2; i <= n && n > 1;) {
+      if (n % i == 0) {
+        System.out.print(i + " ");
+        n /= i;
+      } else i++;
     }
+  }
 }
