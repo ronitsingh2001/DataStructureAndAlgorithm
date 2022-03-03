d7aa42832a2f79d0463589b7a8f979178a813798
unknown <ronit2001krish@gmail.com>
2022-03-03 14:20:19 +0530

    new sawal

--- dynamicProgramming/BuySellStock1Transcation.java
+++ dynamicProgramming/BuySellStock1Transcation.java
@@ -3,23 +3,23 @@ package dynamicProgramming;
 import java.util.Scanner;
 
 public class BuySellStock1Transcation {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] price = new int[n];
-        for (int i = 0; i < n; i++)
-            price[i] = sc.nextInt();
-        sc.close();
-        int lsf = Integer.MAX_VALUE, op = 0, pist = 0;
-        for (int i = 0; i < n; i++) {
-            if (price[i] < lsf) {
-                lsf = price[i];
-            }
-            pist = price[i] - lsf;
-            if (op < pist) {
-                op = pist;
-            }
-        }
-        System.out.println(op);
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] price = new int[n];
+    for (int i = 0; i < n; i++) price[i] = sc.nextInt();
+    sc.close();
+    int lsf = Integer.MAX_VALUE, op = 0, pist = 0;
+    for (int i = 0; i < n; i++) {
+      if (price[i] < lsf) {
+        lsf = price[i];
+      }
+      pist = price[i] - lsf;
+      if (op < pist) {
+        op = pist;
+      }
     }
+    System.out.println(op);
+  }
 }
